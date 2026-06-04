73f7c542538dfb8d12381549868f1b450abc7b38
Naruto-Takahashi <nalt@Nalt.localdomain>
2026-06-05 02:10:28 +0900

    Add Nix & direnv setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Python development environment for Human-Computable-Passwords";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          numpy
+          pandas
+          matplotlib
+          scikit-learn
+          tensorflow
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+          ];
+        };
+      }
+    );
+}
